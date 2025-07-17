a135a43a500a8e68048e7e88c1822cd9357fd043
Artsem <artiom.levitskiy@gmail.com>
2025-07-16 20:09:19 -0400

    Created a food class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle
 import time
 import snake
-
+import food
 screen = turtle.Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
@@ -9,6 +9,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = snake.Snake()
+food = food.Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -16,13 +17,14 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
